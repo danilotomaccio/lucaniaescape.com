b353dd3717e5d360e45eac3aa554b5374797dfc0
danilotomaccio <danilo.tomaccio@gmail.com>
2024-01-26 19:28:34 +0000

    Deploy updates

---
+++ _nuxt/index.P8Lt0Yek.js
@@ -0,0 +1,1 @@
+import{u as r}from"./vue.f36acd1f.1e6LdOh_.js";import{J as f,K as m}from"./entry.wfaly0yj.js";function p(t,e={}){return r(t,{...e,transform:f})}function c(t,e){const{title:n,titleTemplate:s,...o}=t;return r({title:n,titleTemplate:s,_flatMeta:o},{...e,transform(a){const u=m({...a._flatMeta});return delete a._flatMeta,{...a,meta:u}}})}export{c as a,p as u};
